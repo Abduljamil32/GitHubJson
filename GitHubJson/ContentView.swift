97e7009b1c922de5a3e250c8bb54a0aee4f8ed7d
co.abu <anizomxoja@gmail.com>
2022-07-20 13:57:59 +0500

    Initial Commit

---
+++ GitHubJson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubJson
+//
+//  Created by Avaz Mukhitdinov on 20/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubJson/GitHubJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubJsonApp.swift
+//  GitHubJson
+//
+//  Created by Avaz Mukhitdinov on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
